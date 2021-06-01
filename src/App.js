0dc8671d575062fd15befefa03bf104a70e119ce
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-06-01 11:15:32 +0530

    update : add landing page content

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { useState } from "react";
 import "./App.css";
 
 export default function App() {
-  return <h1>Hello React!!</h1>;
+  const [counter, setCounter] = useState(1);
+  return (
+    <div className="App">
+      <h1>Hello React!!</h1>
+      <p>
+        Edit <span>App.js</span> file to see changes
+      </p>
+      <button onClick={() => setCounter((counter) => counter + 1)}>
+        Counter • {counter}{" "}
+      </button>
+    </div>
+  );
 }
